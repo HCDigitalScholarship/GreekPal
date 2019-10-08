1195c1b809581d170fa81d9c6dc8caf4fd703993
Andy <apjanco@gmail.com>
2019-10-08 14:43:44 -0400

    resize image on upload

--- greek_app/models.py
+++ greek_app/models.py
@@ -1,5 +1,8 @@
 import uuid
 from django.db import models
+import PIL.Image
+from PIL import ImageEnhance
+
 
 class Type(models.Model):
     name = models.CharField(max_length=220, blank=True, null=True)
@@ -16,7 +19,9 @@ class Image(models.Model):
 # Create your models here.
 class Symbol(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image = models.ImageField(upload_to='symbols/', blank=True, null=True)
+    image = models.ImageField(upload_to='symbols/', blank=True, null=True, editable=True,)
+    image_height = models.PositiveIntegerField(null=True, blank=True, editable=False, default="150")
+    image_width = models.PositiveIntegerField(null=True, blank=True, editable=False, default=None)
     expansion = models.CharField(max_length=220, blank=True, null=True)
     transcription = models.CharField(max_length=220, blank=True, null=True)
     type = models.ForeignKey(Type, on_delete=models.CASCADE, blank=True, null=True, related_name='symbol_type')
@@ -28,3 +33,15 @@ class Symbol(models.Model):
 
     def __str__(self):
         return self.expansion
+
+    def save(self):
+        if not self.image:
+            return
+
+        super(Symbol, self).save()
+        image = PIL.Image.open(self.image)
+        (width, height) = image.size
+        size = (150, 150)
+        image.thumbnail(size)
+        #image = image.resize(size)
+        image.save(self.image.path)
